70d38e78b5a2ceceda2ce23114f4ff54aca0acc7
ashishyadav83 <ashish@syncarc.net>
2025-01-28 13:32:07 +0530

    initial commit

---
+++ _example/forwarding-zone/main.tf
@@ -0,0 +1,40 @@
+provider "google" {
+  project = "soy-smile-435017-c5"
+  region  = "asia-northeast1"
+  zone    = "asia-northeast1-a"
+}
+
+#####==============================================================================
+##### vpc module call.
+#####==============================================================================
+module "vpc" {
+  source                                    = "git@github.com:SyncArcs/terraform-google-vpc.git?ref=v1.0.1"
+  name                                      = "app"
+  environment                               = "test"
+  routing_mode                              = "REGIONAL"
+  network_firewall_policy_enforcement_order = "AFTER_CLASSIC_FIREWALL"
+}
+
+#####==============================================================================
+##### dns-forwarding-zone module call.
+#####==============================================================================
+module "dns_forwarding_zone" {
+  source                             = "../../"
+  type                               = "forwarding"
+  name                               = "app-test"
+  environment                        = "forwarding"
+  visibility                         = "private"
+  domain                             = var.domain
+  labels                             = var.labels
+  private_visibility_config_networks = [module.vpc.self_link]
+  target_name_server_addresses = [
+    {
+      ipv4_address    = "8.8.8.8",
+      forwarding_path = "default"
+    },
+    {
+      ipv4_address    = "8.8.4.4",
+      forwarding_path = "default"
+    }
+  ]
+}
---
+++ _example/peering-zone/main.tf
@@ -0,0 +1,35 @@
+provider "google" {
+  project = "soy-smile-435017-c5"
+  region  = "asia-northeast1"
+  zone    = "asia-northeast1-a"
+}
+
+#####==============================================================================
+##### vpc module call.
+#####==============================================================================
+module "vpc" {
+  source                                    = "git@github.com:SyncArcs/terraform-google-vpc.git?ref=v1.0.1"
+  name                                      = "app"
+  environment                               = "test"
+  routing_mode                              = "REGIONAL"
+  network_firewall_policy_enforcement_order = "AFTER_CLASSIC_FIREWALL"
+}
+
+
+#####==============================================================================
+##### dns-peering-zone module call.
+#####==============================================================================
+module "dns_peering_zone" {
+  source                             = "../../"
+  type                               = "peering"
+  name                               = "app-test"
+  environment                        = "peering"
+  domain                             = "foo.local."
+  visibility                         = "private"
+  private_visibility_config_networks = [module.vpc.self_link]
+  target_network                     = ""
+  labels = {
+    owner   = "foo"
+    version = "bar"
+  }
+}
